26619ef1b3bb564ca0017c36a008fc5fcab3cdb7
Joey Gough <j.gough@novafutur.com>
2022-04-04 18:38:13 +0100

    create incrment component

---
+++ src/components/Incrementer.svelte
@@ -0,0 +1,14 @@
+<svelte:options tag="mu-custom-incrementer" />
+
+<script>
+	import { count } from "./stores.ts";
+
+	function increment() {
+		// TODO increment the count
+		count.update((n) => n + 1);
+	}
+</script>
+
+<main>
+	<button on:click={increment}> Increment + </button>
+</main>
